ff6de16f283d1e01dbe1b144b04148f9efc0d4b2
Arpit Gogia <arpitgogia@protonmail.com>
2016-08-18 00:42:31 +0530

    UI Changes

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,11 @@ $(document).ready(function() {
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200)
             s = JSON.parse(xhttp.responseText);
-            var html = "<p class='time'>" + s.current.temp_c + String.fromCharCode(176) + "</p>";
+            console.log(s.current);
+            var condition = s.current.condition;
+            var html = "<p class='time'>" + "<img src='" + "http://" + condition.icon.slice(2) + "'>" + s.current.temp_c + String.fromCharCode(176) + "</p>";
+            // html += "<div style='font-size: 20px'" + condition.feelslike_c + "</div>";
+            html += "<p class='time' style='font-size:20px'>" + condition.text + "</p>";
             $('#weather').html(html);
     };
     xhttp.open('GET', "http://api.apixu.com/v1/current.json?key=05fcb9bae03b415baf4145821161708&q=Delhi,IN", true);
