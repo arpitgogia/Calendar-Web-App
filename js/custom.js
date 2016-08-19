e2378ff9f51027b9cc3213092d1c839151d6ebb2
Arpit Gogia <arpitgogia@protonmail.com>
2016-08-19 22:19:57 +0530

    Modal UI Changes

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
     var d = new Date();
     var month = d.getMonth() + 1;
+    
     if(parseInt(month) < 10) {
         var temp = '0';
         temp += month;
         month = temp;
     }
+    
     var date = d.getDate();
     var year = d.getFullYear();
     var dateString = year + '-' + month + '-' + date;
+    
+    //Triggered on each render of the month view, displays the current day in the desired color.
     var markToday = function() {
         var list = document.getElementsByClassName('fc-today');
         for(var i = 0 ; i < list.length ; i++) {
@@ -16,6 +20,25 @@ $(document).ready(function() {
             list[i].style.color = '#f8f8f8';
         }
     };
+    //Triggered on each click of a day, to display the form for creating an event.
+    var dayClickEvent = function(date, jsEvent, view) {
+        date = date.format("dddd, D MMMM YYYY");
+        date = '<span style="color: #ff8787">' + date + '</span>'
+        $(this).webuiPopover({
+            title: date,
+            type: 'html',
+            position: 'right',
+            content: $('#modal').html(),
+            animation: 'fade',
+            closeable: true,
+            onShow: function() {
+                list = document.getElementsByClassName('webui-popover');
+                for(var i = 0 ; i < list.length ; i++)
+                    list[i].style.borderColor = '#ff8787';
+            }
+        });
+    };
+    //Initialize the calendar
     $('#calendar').fullCalendar({
         theme: true,
         header: {
@@ -27,29 +50,24 @@ $(document).ready(function() {
         editable: true,
         eventLimit: true,
         viewRender: markToday,
-        dayClick: function(date, jsEvent, view) {
-            console.log(date);
-            // $(this).popover({
-            //     html: 'true',
-            //     content: $('#modal').html(),
-            //     placement: 'right'
-            // });
-            $(this).webuiPopover({
-                type: 'html',
-                content: '#modal'
-            })
-        }
+        dayClick: dayClickEvent
     });
-    // $('#mini_calendar').dcalendar();
+    
+    //Adjusting the height of the left column.
     $('#left-column').css('height', $(document).height() + "px");
+    
+    //Creating the clock
     var d = new Date();
     var s = d.toTimeString();
     $('#clock').html(s.slice(0, s.indexOf(':') + 3));
+    
     var mProgress = new Mprogress({
         template: 3,
         parent: '#progress'
     });
     mProgress.start();
+    //Retrieving the weather
+    //TO-DO: Fix weather icons according to specifications
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -63,27 +81,19 @@ $(document).ready(function() {
     };
     xhttp.open('GET', "http://api.apixu.com/v1/current.json?key=05fcb9bae03b415baf4145821161708&q=Delhi,IN", true);
     xhttp.send();
+    
     var list = document.getElementsByClassName('fc-other-month');
     for(var i = 0 ; i < list.length ; i++) {
         list[i].style.background = '#f8f8f8';
         list[i].style.color = '#b3b3b3';
     }
+    
     list = document.getElementsByClassName('fc-today');
     for(var i = 0 ; i < list.length ; i++) {
         list[i].style.background = '#2ed39e';
         list[i].style.color = '#f8f8f8';
     }
-    // list = document.getElementsByTagName('td');
-    // for(var i = 0 ; i < list.length ; i++) {
-    //     list[i].style.textalign = 'left';
-    // }
+
     $('td').css('text-align', 'left');
     $('td').css('margin-top', '10px');
-    // $('.fc-day').not('.fc-other-month').click(function(e) {
-    //     $(this).popover({
-    //         html: 'true',
-    //         placement: 'right',
-    //         content: '<input type="text"></input>'
-    //     })
-    // })
 })
